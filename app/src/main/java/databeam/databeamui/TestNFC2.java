d2d04e246dd0b7ab453aafd2c9bb01784abc2e48
Matt <mmott@udel.edu>
2017-11-02 17:36:22 -0400

    testnfc update

--- app/src/main/java/databeam/databeamui/TestNFC2.java
+++ app/src/main/java/databeam/databeamui/TestNFC2.java
@@ -18,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
+import android.view.MenuItem;
 
 import databeam.databeamui.R;
 
