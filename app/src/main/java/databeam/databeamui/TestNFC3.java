a0408ab4017b52b1079472e4af2fd93f59d8019e
Justin Jagielski <jjagiels@udel.edu>
2017-11-08 14:59:39 -0500

    Begun adding NFC support

---
+++ app/src/main/java/databeam/databeamui/TestNFC3.java
@@ -0,0 +1,43 @@
+package databeam.databeamui;
+
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+
+import java.io.UnsupportedEncodingException;
+import javacard.framework.*;
+
+/**
+ * Created by justin on 11/7/17.
+ */
+
+public class TestNFC3 extends HostApduService {
+
+
+    @Override
+    public byte[] processCommandApdu(byte[] bytes, Bundle bundle) {
+        switch(bytes[1]){
+            case 0x01:
+                return "Command 1 Selected!".getBytes();
+                break;
+            case 0x02:
+                return strToHex(userInput);
+                break;
+            default:
+        }       return "Incorrect/Missing Command".getBytes();
+
+
+    }
+
+    @Override
+    public void onDeactivated(int i) {
+        //Tell the app to display a infobox that mentions the reason for deactivation.
+    }
+
+    public byte[] strToHex(String userInput) {
+        try {
+            return userInput.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
