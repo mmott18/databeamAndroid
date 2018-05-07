1523140577e53cdc3a4ae4b8fbd863c7e5c5b64a
Justin Jagielski <jjagiels@udel.edu>
2018-05-07 17:23:22 -0400

    Moved classes to conform with serialization requirements

---
+++ app/src/main/java/dataClasses/BasicInfo.java
@@ -0,0 +1,46 @@
+package dataClasses;
+
+import java.io.Serializable;
+
+/**
+ * Created by justin on 2/19/18.
+ */
+
+public class BasicInfo implements Serializable {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String DOB;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
